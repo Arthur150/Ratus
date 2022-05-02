b646db9dab24a63ed0522561d6432ce15753342e
Arthur <arthur.chevauchet@gmail.com>
2022-05-02 13:57:25 +0200

    Initial commit

---
+++ app/src/main/java/fr/univ/lyon1/lpiem/ratus/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.univ.lyon1.lpiem.ratus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
