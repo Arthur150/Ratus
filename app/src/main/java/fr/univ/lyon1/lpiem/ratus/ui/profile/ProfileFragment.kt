5c0acba019228953433a71c88dc8b2e29dadb8c6
Antoine Pannecoucke <antoine.pannecoucke@etu.univ-lyon1.fr>
2022-05-13 09:05:35 +0200

    generate qr code

--- app/src/main/java/fr/univ/lyon1/lpiem/ratus/ui/profile/ProfileFragment.kt
+++ app/src/main/java/fr/univ/lyon1/lpiem/ratus/ui/profile/ProfileFragment.kt
@@ -1,6 +1,8 @@
 package fr.univ.lyon1.lpiem.ratus.ui.profile
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,9 +10,12 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import coil.load
 import com.google.firebase.auth.FirebaseAuth
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.qrcode.QRCodeWriter
 import fr.univ.lyon1.lpiem.ratus.LoginActivity
 import fr.univ.lyon1.lpiem.ratus.R
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -25,11 +30,35 @@ class ProfileFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
 
-        viewModel.user.observe(viewLifecycleOwner) {
-            view.findViewById<TextView>(R.id.username).text = it.username
-            view.findViewById<TextView>(R.id.userUID).text = it.uid
-            view.findViewById<ImageView>(R.id.userThumbnail).load(it.thumbnail)
+        viewModel.user.observe(viewLifecycleOwner) { user ->
+            view.findViewById<TextView>(R.id.username).text = user.username
+            view.findViewById<TextView>(R.id.userUID).text = user.uid
+            view.findViewById<ImageView>(R.id.userThumbnail).load(user.thumbnail)
+
+            view.findViewById<ImageView>(R.id.qr_codeCreator).setOnClickListener {
+                val qrCodeView = ImageView(requireContext())
+
+                val size = 900 //pixels
+                val qrCodeContent = user.uid
+                val bits = QRCodeWriter().encode(qrCodeContent, BarcodeFormat.QR_CODE, size, size)
+                val bitmap =  Bitmap.createBitmap(size, size, Bitmap.Config.RGB_565).also {
+                    for (x in 0 until size) {
+                        for (y in 0 until size) {
+                            it.setPixel(x, y, if (bits[x, y]) Color.BLACK else Color.WHITE)
+                        }
+                    }
+                }
+
+                qrCodeView.setImageBitmap(bitmap)
+                val alertDialog = AlertDialog.Builder(requireContext())
+                    .setView(qrCodeView)
+                    .create()
+                alertDialog.show()
+            }
         }
+
+
+
 
         viewModel.getUser()
 
